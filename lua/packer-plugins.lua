00d03099907ed6456d22a18fc20e49df740fc1d5
Jack Gitter <gitter.j@husky.neu.edu>
2022-09-15 19:37:09 -0400

    everything is working

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -12,6 +12,7 @@ return require('packer').startup(function()
 	use 'hrsh7th/cmp-nvim-lua'
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'saadparwaiz1/cmp_luasnip'
+	use 'L3MON4D3/LuaSnip'
 	-- lsp plugins
 	use 'neovim/nvim-lspconfig'
 	use 'williamboman/nvim-lsp-installer'
