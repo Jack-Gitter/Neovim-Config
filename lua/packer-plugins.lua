fe96003815696af2ffa62dc42049eed087ab92af
Jack Gitter <gitter.j@husky.neu.edu>
2022-09-28 14:30:46 -0400

    omg this config is good

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -22,6 +22,7 @@ return require('packer').startup(function()
 	use {'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	requires = { {'nvim-lua/plenary.nvim'}}}
 	use 'psliwka/vim-smoothie'
+	use 'kdheepak/lazygit.nvim'
 	
 end)
 
