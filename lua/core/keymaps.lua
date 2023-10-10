4d9565b7fdb43f9b58a10f1f3989e3d5073a76a2
JackGitter <gitter.j@northeastern.edu>
2023-10-09 23:05:54 -0400

    nice

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,15 +1,17 @@
 vim.opt.showcmd = true
 vim.opt.autowrite = true
 vim.opt.autoread = true
-vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set('i', 'kj', '<Esc>')
-vim.keymap.set('n', 'git', ':FloatermNew LazyGit<CR>')
-vim.keymap.set('n', 'winl', ':wincmd h<CR>')
-vim.keymap.set('n', 'winr', ':wincmd l<CR>')
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.updatetime = 50
+vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set('i', 'kj', '<Esc>')
+vim.keymap.set('n', 'git', ':FloatermNew LazyGit<CR>')
+vim.keymap.set('n', 'winl', ':wincmd h<CR>')
+vim.keymap.set('n', 'winr', ':wincmd l<CR>')
